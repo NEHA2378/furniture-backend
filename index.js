36c6ecca98e15faeceed5bb3c2c958be453b8b1b
NEHA2378 <121502417+NEHA2378@users.noreply.github.com>
2026-04-30 16:06:16 +0530

    fix dotenv config and move to dependencies

--- index.js
+++ index.js
@@ -10,7 +10,34 @@ const { webRoutes } = require("./App/routes/web/webRoutes");
 let App = express()
 
 App.use(cors())
+require("dotenv").config()
 
+let express = require("express")
+const { adminRoutes } = require("./App/routes/admin/adminRoutes")
+let cors = require("cors")
+const mongoose = require('mongoose');
+const { webRoutes } = require("./App/routes/web/webRoutes");
+
+let App = express()
+
+App.use(cors())
+App.use(express.json())
+App.use(express.urlencoded({ extended: true }))
+
+App.use('/uploads', express.static('uploads'))
+App.use('/uploads/product', express.static('uploads/product'))
+App.use("/uploads/category", express.static("uploads/category"))
+App.use("/uploads/subcategory", express.static("uploads/subCategory"))
+App.use("/uploads/subsubcategory", express.static("uploads/subsubCategory"))
+
+App.use("/admin", adminRoutes)
+App.use("/web", webRoutes)
+
+App.listen(process.env.PORT || 8000, () => {
+    mongoose.connect(process.env.MONGODB_URI)
+        .then(() => console.log('Connected!'));
+    console.log("Server Started", process.env.PORT);
+})
 App.use(express.json())
 App.use(express.urlencoded({ extended: true }))
 
@@ -28,6 +55,6 @@ App.use("/web", webRoutes)
 App.listen(process.env.PORT || 8000, () => {
 
     mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('Connected!'));
+        .then(() => console.log('Connected!'));
 
 })
