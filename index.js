068385eed6eafbf17324459d64a4a9540720adb0
NEHA2378 <121502417+NEHA2378@users.noreply.github.com>
2026-04-30 16:08:58 +0530

    fix duplicate express declaration

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-let express = require("express")
-const { adminRoutes } = require("./App/routes/admin/adminRoutes")
-require("dotenv").config()
-let cors = require("cors")
-
-// Using Node.js `require()`
-const mongoose = require('mongoose');
-const { webRoutes } = require("./App/routes/web/webRoutes");
-
-let App = express()
-
-App.use(cors())
 require("dotenv").config()
 
 let express = require("express")
@@ -37,24 +25,4 @@ App.listen(process.env.PORT || 8000, () => {
     mongoose.connect(process.env.MONGODB_URI)
         .then(() => console.log('Connected!'));
     console.log("Server Started", process.env.PORT);
-})
-App.use(express.json())
-App.use(express.urlencoded({ extended: true }))
-
-App.use('/uploads', express.static('uploads'))
-
-App.use('/uploads/product', express.static('uploads/product'))
-App.use("/uploads/category", express.static("uploads/category"))
-App.use("/uploads/subcategory", express.static("uploads/subCategory"))
-App.use("/uploads/subsubcategory", express.static("uploads/subsubCategory"))
-
-App.use("/admin", adminRoutes)
-
-App.use("/web", webRoutes)
-
-App.listen(process.env.PORT || 8000, () => {
-
-    mongoose.connect(process.env.MONGODB_URI)
-        .then(() => console.log('Connected!'));
-
 })
